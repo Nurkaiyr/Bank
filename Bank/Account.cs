f64ab92490980ec53362bb9a923799b770b2ee21
Едиге Нуркайыр Маратулы <@corp.itstep>
2018-05-04 11:37:08 +0600

    hw done

---
+++ Bank/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Account
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Money { get; set; }
+        int _sum;
+
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+            }
+        }
+    }
+}
+
